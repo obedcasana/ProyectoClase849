b2e7ce48615a476647f5b1a4c3cdee944c25032f
obedcasana <obecasana@unitec.edu>
2026-05-05 12:37:45 -0600

    CambiosClase3

--- MiAppMovil/App.tsx
+++ MiAppMovil/App.tsx
@@ -2,17 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CustomButton from "./src/components/CustomButton";
 import CustomInput from "./src/components/CustomInput";
+import { useState } from "react";
+
 
 export default function App() {
+  //definicion de una variable de estado en ReactN
+const [inputText, setInputText] = useState("maria");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <CustomInput type={"number"} placeholder={""} value={""} onChange={function (text: string): void {} }/>
+      
+      <CustomInput 
+      type={"number"} 
+      placeholder={""} 
+      value={inputText} 
+      onChange={setInputText}
+      />
+ 
       <CustomButton
         title={"App"}
         onPress={() => {
-          console.log("Press desde boton");
+          console.log("Press desde boton App");
         }}
       />
        <CustomButton
@@ -25,7 +37,7 @@ export default function App() {
        <CustomButton
         title={"Tertiary Button"}
         onPress={() => {
-          console.log("Press desde boton Terciario");
+          console.log("Press desde boton Secundario");
         }}
         variant="tertiary"
       />
@@ -39,5 +51,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    
   },
 });
--- MiAppMovil/src/components/CustomInput.tsx
+++ MiAppMovil/src/components/CustomInput.tsx
@@ -1,26 +1,72 @@
 import { Ionicons } from "@expo/vector-icons";
-import { TextInput, TouchableOpacity, View, Text} from "react-native";
+import { useState } from "react";
+import { TextInput, TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
-type Props={
+type Props = {
     type: "text" | "email" | "password" | "number";
     placeholder: string;
     value: string;
     onChange: (text: string) => void;
 }
 
-export default function CustomInput({type, placeholder, value, onChange}: Props){
-    return(
-        <View>
-            <TextInput
-            placeholder={placeholder}
-            value={value}
-            onChangeText={onChange}
-            />
+export default function CustomInput ({type, placeholder, value, onChange}: Props){
+    const [isSecureText, setIsSecureText] = useState(true);
 
-            <TouchableOpacity>
-                <Text>Icon Button</Text>
-                <Ionicons name={"eye"} size={14}/>
-            </TouchableOpacity>
-        </View>
+    const icon : typeof Ionicons["name"] | undefined = 
+        type === "email" ? 'at' : 
+            type === "password" ? 'lock-closed' : undefined
+
+    return(
+        //wrapper
+         <View style={styles.wrapper}>
+            <View style={styles.inputContainer}>
+                <Ionicons name={icon as any} size={20} color ="#00000"/>
+                <TextInput 
+                    placeholder={placeholder}
+                    value={value}
+                    onChangeText={onChange}
+                    style={styles.input}
+                    secureTextEntry={isSecureText}
+                />
+                <TouchableOpacity
+                onPress={
+                    ()=>{
+                        setIsSecureText(!isSecureText)
+                    }
+                }
+                >
+                    <Ionicons name={isSecureText ? "eye" : "eye-off"} size={22} />
+                </TouchableOpacity>
+            </View>
+            <Text>Probando Wrapper</Text>
+         </View>
+
     );
 }
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginBottom: 10,
+    },
+    inputContainer: {
+        //distribucion de componentes
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+
+        //estilizacion de input
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 9,
+        backgroundColor: "#f0f0f0",
+        paddingLeft: 20,
+
+    },
+    input: {
+        width: '80%',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+      
+    }
+})
